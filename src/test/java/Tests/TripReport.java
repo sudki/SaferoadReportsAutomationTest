d5065e69584a0d8e433ddac4d7719ed37b83d975
Sudki <sudki.kfa@gmail.com>
2026-02-01 15:47:06 +0300

    update Allure report

--- src/test/java/Tests/TripReport.java
+++ src/test/java/Tests/TripReport.java
@@ -10,35 +10,33 @@ import org.testng.annotations.Test;
 import Pages.LoginPage;
 import Pages.ReportsPage;
 
-public class WHM_Reports extends BaseTest {
+
+public class TripReport extends BaseTest {
 
     @Test
-    public void testWHMreportHasData(){
+    public void TripReportTest() {
 
         LoginPage loginPage = new LoginPage(driver);
         loginPage.login(TestConfig.USERNAME,TestConfig.PASSWORD);
 
         ReportsPage reportsPage = new ReportsPage(driver);
-        reportsPage.openWHM_Reports();
+        reportsPage.openTripReport();
         reportsPage.selectAllVehicles();
         reportsPage.clickShowReports();
 
-
         boolean hasData = reportsPage.isReportHasData();
         int rows = driver.findElements(By.cssSelector(".ag-center-cols-container .ag-row.ag-row-level-0")).size();
-        Allure.addAttachment("Records Count (WHM report)", String.valueOf(rows));
-        System.out.println(" WHM Report hasData = " + hasData + " | rows = " + rows);
+        Allure.addAttachment("Records Count (Trip Report)", String.valueOf(rows));
+        System.out.println("Trip Report hasData = " + hasData + " | rows = " + rows);
         if (rows == 0) {
             Allure.addAttachment(
-                    "WHM Report Status",
+                    "Trip Report Status",
                     "Report generated successfully but returned NO DATA "
             );
-            System.out.println(" WHM Report generated but has no data");
+            System.out.println("Trip Report generated but has no data");
         }
 
         Assert.assertTrue(true, "Trip Report page loaded successfully");
-
     }
+
 }
-
-
--- src/test/java/Tests/WHM_Report.java
+++ src/test/java/Tests/WHM_Report.java
@@ -10,33 +10,35 @@ import org.testng.annotations.Test;
 import Pages.LoginPage;
 import Pages.ReportsPage;
 
-
-public class TripReportTest extends BaseTest {
+public class WHM_Report extends BaseTest {
 
     @Test
-    public void TripReportTest() {
+    public void WHMReportTest(){
 
         LoginPage loginPage = new LoginPage(driver);
         loginPage.login(TestConfig.USERNAME,TestConfig.PASSWORD);
 
         ReportsPage reportsPage = new ReportsPage(driver);
-        reportsPage.openTripReport();
+        reportsPage.openWHM_Reports();
         reportsPage.selectAllVehicles();
         reportsPage.clickShowReports();
 
+
         boolean hasData = reportsPage.isReportHasData();
         int rows = driver.findElements(By.cssSelector(".ag-center-cols-container .ag-row.ag-row-level-0")).size();
-        Allure.addAttachment("Records Count (Trip Report)", String.valueOf(rows));
-        System.out.println("Trip Report hasData = " + hasData + " | rows = " + rows);
+        Allure.addAttachment("Records Count (WHM report)", String.valueOf(rows));
+        System.out.println(" WHM Report hasData = " + hasData + " | rows = " + rows);
         if (rows == 0) {
             Allure.addAttachment(
-                    "Trip Report Status",
+                    "WHM Report Status",
                     "Report generated successfully but returned NO DATA "
             );
-            System.out.println("Trip Report generated but has no data");
+            System.out.println(" WHM Report generated but has no data");
         }
 
         Assert.assertTrue(true, "Trip Report page loaded successfully");
-    }
 
+    }
 }
+
+
