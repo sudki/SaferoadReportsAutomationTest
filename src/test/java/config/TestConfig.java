bb6f8bac5534c33c0702e3b83ebfee29712d9443
Sudki <sudki.kfa@gmail.com>
2026-03-08 10:13:16 +0300

    update 26/2

--- src/test/java/config/TestConfig.java
+++ src/test/java/config/TestConfig.java
@@ -3,5 +3,5 @@ package config;
 public class TestConfig {
 
     public static final String USERNAME = "Demo1234";
-    public static final String PASSWORD = "O@2023d";
+    public static final String PASSWORD = "O@2026d";
 }
